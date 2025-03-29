690317d57da78096eebb2da5272ba5c9be56da8e
techlust <t3chlust@gmail.com>
2025-03-29 09:40:01 +0500

    Change namespace in README.md

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'shusha/dynamicobjects',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c2f86d60f7414c8d7e22a4f476d2b49f90b0a6eb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'shusha/dynamicobjects' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c2f86d60f7414c8d7e22a4f476d2b49f90b0a6eb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
